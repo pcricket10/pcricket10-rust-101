784b34b4c84cdc0da36902e14e95a38d7bc96eee
Philip McCrickard <pcricket10@gmail.com>
2023-07-05 16:54:21 -0400

    a2

--- activities/src/bin/a2.rs
+++ activities/src/bin/a2.rs
@@ -8,4 +8,18 @@
 // * Use a function to display the result
 // * Use the "{:?}" token in the println macro to display the result
 
-fn main() {}
+fn main() {
+    let add1: i32 = add(1, 2); //3
+    let add2: i32 = add(4, 3); //7
+    let add3: i32 = add(2, -5); // -3
+    display_result(add1);
+    display_result(add2);
+    display_result(add3);
+}
+fn display_result(result: i32) {
+    println!("{result:?}");
+}
+
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
