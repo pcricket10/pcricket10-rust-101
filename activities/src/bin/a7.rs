68570ed649c8d2429b73e31b325b350cd214ea8f
Philip McCrickard <pcricket10@gmail.com>
2023-07-07 16:55:32 -0400

    a7

--- activities/src/bin/a7.rs
+++ activities/src/bin/a7.rs
@@ -10,4 +10,20 @@
 // * Use a match expression to determine which color
 //   name to print
 
-fn main() {}
+enum Color {
+    Red,
+    Blue,
+    Green,
+    Yellow,
+}
+fn main() {
+    print_color_name(Color::Yellow);
+}
+fn print_color_name(color: Color) {
+    match color {
+        Color::Red => println!("you have chosen red"),
+        Color::Blue => println!("you have chosen blue"),
+        Color::Green => println!("you have chosen green"),
+        Color::Yellow => println!("you have chosen yellow"),
+    }
+}
