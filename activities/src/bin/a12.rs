53bd54097fa1750bf5fb6fede5d99ae5ca5dfea7
Philip McCrickard <pcricket10@gmail.com>
2023-07-18 16:55:28 -0400

    a12

--- activities/src/bin/a12.rs
+++ activities/src/bin/a12.rs
@@ -10,4 +10,62 @@
 // * Implement functionality on the box struct to create a new box
 // * Implement functionality on the box struct to print the characteristics
 
-fn main() {}
+struct Box {
+    dimensions: Dimensions,
+    weight: f64,
+    color: Color,
+}
+struct Dimensions {
+    width: f64,
+    height: f64,
+    depth: f64,
+}
+impl Dimensions {
+    fn print(&self) {
+        println!("width: {}", self.width);
+        println!("height: {}", self.height);
+        println!("depth: {}", self.depth);
+    }
+}
+enum Color {
+    Red,
+    Green,
+    Blue,
+}
+impl Color {
+    fn print(&self) {
+        match self {
+            Color::Red => println!("Red"),
+            Color::Green => println!("Green"),
+            Color::Blue => println!("Blue"),
+        }
+    }
+}
+
+impl Box {
+    fn new(weight: f64, color: Color, dimensions: Dimensions) -> Self {
+        Self {
+            weight,
+            color,
+            dimensions,
+        }
+    }
+
+    fn print(&self) {
+        self.color.print();
+        self.dimensions.print();
+        println!("weight: {}", self.weight);
+    }
+}
+fn main() {
+    let new_box = Box {
+        dimensions: Dimensions {
+            width: 1.2,
+            height: 3.4,
+            depth: 5.6,
+        },
+        weight: 2.2,
+        color: Color::Green,
+    };
+    new_box.print();
+}
--- activities/src/bin/a8.rs
+++ activities/src/bin/a8.rs
@@ -9,4 +9,39 @@
 // * Use a function to print out the drink flavor and ounces
 // * Use a match expression to print the drink flavor
 
-fn main() {}
+enum DrinkFlavors {
+    Grape,
+    Lemon,
+    Cherry,
+}
+
+struct Drink {
+    flavor: DrinkFlavors,
+    fluid_ounces: f32,
+}
+fn main() {
+    let drink1 = Drink {
+        flavor: DrinkFlavors::Grape,
+        fluid_ounces: 12.0,
+    };
+    let drink2 = Drink {
+        flavor: DrinkFlavors::Lemon,
+        fluid_ounces: 420.69,
+    };
+    let drink3 = Drink {
+        flavor: DrinkFlavors::Cherry,
+        fluid_ounces: 0.1,
+    };
+
+    print_drinks(drink1);
+    print_drinks(drink2);
+    print_drinks(drink3);
+}
+
+fn print_drinks(drink: Drink) {
+    match drink.flavor {
+        DrinkFlavors::Cherry => println!("cherry drink has {} fluid ounces", drink.fluid_ounces),
+        DrinkFlavors::Grape => println!("grape drink has {} fluid ounces", drink.fluid_ounces),
+        DrinkFlavors::Lemon => println!("lemon drink has {} fluid ounces", drink.fluid_ounces),
+    }
+}
