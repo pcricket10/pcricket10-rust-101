ec8d10032b6b00c49315730bf9f1bd25d4499dbd
Philip McCrickard <pcricket10@gmail.com>
2023-07-18 18:44:30 -0400

    a14

--- activities/src/bin/a14.rs
+++ activities/src/bin/a14.rs
@@ -10,5 +10,38 @@
 // * Iterate through the vector using a for..in loop
 // * Use an if expression to determine which person's info should be printed
 // * The name and colors should be printed using a function
-
-fn main() {}
+struct Person {
+    age: u16,
+    name: String,
+    favorite_color: String,
+}
+impl Person {
+    fn print(&self) {
+        println!("{}", self.name);
+        println!("{}", self.favorite_color);
+    }
+}
+fn main() {
+    let people = vec![
+        Person {
+            age: 11,
+            name: "Bob".to_string(),
+            favorite_color: "blue".to_string(),
+        },
+        Person {
+            age: 4,
+            name: "Joe".to_string(),
+            favorite_color: "gray".to_string(),
+        },
+        Person {
+            age: 10,
+            name: "Billy".to_string(),
+            favorite_color: "purple".to_string(),
+        },
+    ];
+    for person in people {
+        if person.age <= 10 {
+            person.print();
+        }
+    }
+}
