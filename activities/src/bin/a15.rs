caf3d0cc7f38c9da5e08223f5a60519faced7ca6
Philip McCrickard <pcricket10@gmail.com>
2023-07-20 15:17:33 -0400

    a15

--- activities/src/bin/a15.rs
+++ activities/src/bin/a15.rs
@@ -10,5 +10,40 @@
 // * Use an enum for the tickets with data associated with each variant
 // * Create one of each ticket and place into a vector
 // * Use a match expression while iterating the vector to print the ticket info
+#[derive(Debug)]
+enum Ticket {
+    Backstage(f64, String),
+    Vip(f64, String),
+    Standard(f64),
+}
+// struct Ticket {
+//     name: String,
+//     ticket_type: TicketType,
+//     ticket_price: f32,
+// }
+// impl Ticket {
+//     fn print(&self) {
+//         println!("{:?}", self.name);
+//         println!("{:?}", self.ticket_type);
+//         println!("{:?}", self.ticket_price);
+//     }
+// }
 
-fn main() {}
+fn main() {
+    let tickets: Vec<Ticket> = vec![
+        Ticket::Backstage(1000.00, "Joe".to_owned()),
+        Ticket::Vip(80.00, "Bob".to_owned()),
+        Ticket::Standard(60.00),
+    ];
+    for ticket in tickets {
+        match ticket {
+            Ticket::Backstage(price, holder) => {
+                println!("ticket holder: {}, ticket price: {}", holder, price)
+            }
+            Ticket::Vip(price, holder) => {
+                println!("ticket holder: {}, ticket price: {}", holder, price)
+            }
+            Ticket::Standard(price) => println!("ticket price: {}", price),
+        }
+    }
+}
