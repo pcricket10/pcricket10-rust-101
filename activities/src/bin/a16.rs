46cdf6b1d28e7a8ca9f6a07cdee740b93d36d399
Philip McCrickard <pcricket10@gmail.com>
2023-07-24 15:49:12 -0400

    a16

--- activities/src/bin/a16.rs
+++ activities/src/bin/a16.rs
@@ -8,4 +8,29 @@
 // * Use a struct containing the student's name and locker assignment
 // * The locker assignment should use an Option<i32>
 
-fn main() {}
+struct Locker {
+    student_name: String,
+    locker_number: Option<i32>,
+}
+fn main() {
+    let locker1 = Locker {
+        student_name: "Bob".to_owned(),
+        locker_number: Some(12345),
+    };
+    let locker2 = Locker {
+        student_name: "Billy".to_owned(),
+        locker_number: None,
+    };
+    println!("Student Name: {}", locker1.student_name);
+
+    match locker1.locker_number {
+        Some(locker_number) => println!("locker number: {}", locker_number),
+        None => println!("Locker number not specified"),
+    }
+    println!("Student Name: {}", locker2.student_name);
+
+    match locker2.locker_number {
+        Some(locker_number) => println!("locker number: {}", locker_number),
+        None => println!("Locker number not specified"),
+    }
+}
