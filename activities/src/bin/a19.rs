dc3865d7a51368cc8eb68ebe8d2ddb20d2535d66
Philip McCrickard <pcricket10@gmail.com>
2023-08-02 15:30:51 -0400

    a19

--- activities/src/bin/a19.rs
+++ activities/src/bin/a19.rs
@@ -13,4 +13,22 @@
 // Notes:
 // * Use a HashMap for the furniture store stock
 
-fn main() {}
+use std::collections::HashMap;
+
+fn main() {
+    let mut stock = HashMap::new();
+    stock.insert("Chairs", 5);
+    stock.insert("Beds", 3);
+    stock.insert("Tables", 2);
+    stock.insert("Couches", 0);
+    let mut total = 0;
+
+    for (item, quantity) in stock.iter() {
+        match quantity {
+            0 => println!("{} are out of stock", item),
+            _ => println!("There are {} {} in stock", quantity, item),
+        }
+        total += quantity;
+    }
+    println!("There are {} items in stock", total);
+}
